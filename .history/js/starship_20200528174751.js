7842e18e301b9e0fd893c7fc6bda00877d5ba780
expertsumyua <expert.sumy.ua@gmail.com>
2020-05-28 17:51:52 +0300

    new

--- .history/js/starship_20200528174751.js
+++ .history/js/starship_20200528174751.js
@@ -57,7 +57,7 @@ function moveBullet(thisBullet) {
 	thisBullet.style.top = starship.offsetTop - 100 + "px";
 	thisBullet.style.left = starship.offsetLeft + "px";
 
-	let timerBullet = setInterval(function () {
+	var timerBullet = setInterval(function () {
 		thisBullet.style.top = 0 + "px";
 		// если пуля вышла за пределыполя=> 
 		if (thisBullet.offsetTop < 1) {	// удуляем пулю
